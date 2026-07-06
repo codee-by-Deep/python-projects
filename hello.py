677e1104dbbb3818ba34e950fa0af7261730c039
codee-by-Deep <d25603245@gmail.com>
2026-07-06 14:53:33 +0530

    Removed unnecessary files

--- hello.py
+++
@@ -1,8 +0,0 @@
-with open("expenses.txt","r") as f:
-    lines = f.readlines()
-total = 0
-
-for line in lines:
-    part = line.strip().split(",")
-    total = total + int(part[1])
-    print(total) 
